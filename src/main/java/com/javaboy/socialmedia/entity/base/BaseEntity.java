f15ba62f4f935d0f256298fbd77de791e86b37b8
alijavaboy <ali77erfagh99@gmail.com>
2022-04-30 15:03:43 +0430

    add base Entity || model

---
+++ src/main/java/com/javaboy/socialmedia/entity/base/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.javaboy.socialmedia.entity.base;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class BaseEntity<ID extends Serializable> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(ID id) {
+        this.id = id;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+}
