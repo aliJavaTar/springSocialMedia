7f1b00147fdafaf1f68500ee7b5b6b1cd46fc1ec
alijavaboy <ali77erfagh99@gmail.com>
2022-04-30 15:20:04 +0430

    add baseServiceImpl

--- src/main/java/com/javaboy/socialmedia/service/base/impl/BaseServiceImpl.java
+++ src/main/java/com/javaboy/socialmedia/service/base/impl/BaseServiceImpl.java
@@ -1,4 +1,42 @@
 package com.javaboy.socialmedia.service.base.impl;
 
-public class BaseServiceImpl {
+import com.javaboy.socialmedia.entity.base.BaseEntity;
+import com.javaboy.socialmedia.service.base.BaseService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class BaseServiceImpl<E extends BaseEntity<ID>, ID extends Serializable, R extends JpaRepository<E, ID>>
+        implements BaseService<E, ID> {
+    private final R baseRepository;
+
+    public BaseServiceImpl(R baseRepository) {
+        this.baseRepository = baseRepository;
+    }
+
+    @Override
+    public E create(E e) {
+        return baseRepository.save(e);
+    }
+
+    @Override
+    public E update(E e) {
+        return baseRepository.save(e);
+    }
+
+    @Override
+    public void deleteById(ID id) {
+        baseRepository.deleteById(id);
+    }
+
+    @Override
+    public E findById(ID id) {
+        return baseRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<E> findAll() {
+        return baseRepository.findAll();
+    }
 }
